b042cb8fea52fa8ed46dcdeed0e7c6c7b0a6cbad
Jin Ho Hyun <hjho0304@gmail.com>
2021-12-28 21:13:01 +0900

    구글로그인 구현

--- src/main/java/com/zino/book/springboot/web/IndexController.java
+++ src/main/java/com/zino/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.zino.book.springboot.web;
 
+import com.zino.book.springboot.config.auth.dto.SessionUser;
 import com.zino.book.springboot.service.posts.PostsService;
 import com.zino.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,20 +10,27 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
     }
 
-    private final PostsService postsService;
-
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
